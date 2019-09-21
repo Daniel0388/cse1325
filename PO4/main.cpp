ff15bd6c5988ace10cbbf3eaad010e0860c3c031
Daniel0388 <daniel.vargas4@mavs.uta.edu>
2019-09-20 23:12:39 -0500

    Cleaned up main.cpp.

--- PO4/main.cpp
+++ PO4/main.cpp
@@ -56,27 +56,27 @@ int main() {
 
 void print() {
 
-std::cout << "\t=========================" << std::endl;
-std::cout << "\t   The CSE1325 Express" << std::endl;
-std::cout << "\t=========================" << std::endl;
-std::cout << std::endl;
+	std::cout << "\t=========================" << std::endl;
+	std::cout << "\t   The CSE1325 Express" << std::endl;
+	std::cout << "\t=========================" << std::endl;
+	std::cout << std::endl;
 
-//code to print train goes here
-//code to print train goes here
-//code to print train goes here
+	//code to print train goes here
+	//code to print train goes here
+	//code to print train goes here
 
-std::cout << std::endl;
-std::cout << "Minutes |\t1\t  5\t    15\t      30\t60" << std::endl;
-std::cout << "--------|---------|---------|---------|---------|---------|" << std::endl;
-std::cout << "Speed/s |---------|---------|---------|---------|---------|" << std::endl;
-std::cout << std::endl;
-std::cout << "1 - Add a locomotive" << std::endl;
-std::cout << "2 - Add an empty coach" << std::endl;
-std::cout << "9 - Clear the train" << std::endl;
-std::cout << "0 - Exit" << std::endl;
-std::cout << std::endl;
-std::cout << "Command?" << std::endl;
-std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << "Minutes |\t1\t  5\t    15\t      30\t60" << std::endl;
+	std::cout << "--------|---------|---------|---------|---------|---------|" << std::endl;
+	std::cout << "Speed/s |---------|---------|---------|---------|---------|" << std::endl;
+	std::cout << std::endl;
+	std::cout << "1 - Add a locomotive" << std::endl;
+	std::cout << "2 - Add an empty coach" << std::endl;
+	std::cout << "9 - Clear the train" << std::endl;
+	std::cout << "0 - Exit" << std::endl;
+	std::cout << std::endl;
+	std::cout << "Command?" << std::endl;
+	std::cout << std::endl;
 
 };
 
