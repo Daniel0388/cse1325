edb4d2df6e61af746fcd4b634d57ef82808c2ff8
Daniel0388 <54421243+Daniel0388@users.noreply.github.com>
2019-11-24 15:23:24 -0600

    Added save and load feature.

---
+++ P11/shelter.h
@@ -0,0 +1,34 @@
+#ifndef __SHELTER_H
+#define __SHELTER_H
+
+#include "animal.h"
+#include "client.h"
+#include <vector>
+
+class Shelter {
+  public:
+    Shelter(std::string name);
+    std::string name();
+    void set_name(std::string name);
+
+    void add_animal(Animal& animal);
+    int num_animals();
+    Animal& animal(int index); 
+
+    void add_client(Client& client);
+    int num_clients();
+    Client& client(int index);
+
+    void adopt(Client& client, Animal& animal);
+
+    std::string get_filename();
+    void set_filename(std::string filename);
+  private:
+    std::string _name;
+    std::vector<Animal*> _available;
+    std::vector<Client>  _clients;
+    std::string _filename;
+};
+
+#endif
+
